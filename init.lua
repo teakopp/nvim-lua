d3ef959fa4f8f1bfc831a63777d7eebc34eb0562
Travis Kopp <teakopp@protonmail.com>
2023-09-28 10:59:26 -0700

    change up a whole lot of stuff and reconfigure all my settings

--- init.lua
+++ init.lua
@@ -1,39 +1,143 @@
-require('plugins')
-require'lspconfig'.pyright.setup{}
+local plugins = require('plugins') 
 
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- Bootstrap Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- Load Lazy
+local lazy = require("lazy")
+
+-- Lazy setup
+lazy.setup(plugins, {
+  termguicolors = true,
+  disable_netrw = true,
+})
+
+-- Load LSP configurations
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.gopls.setup{}
+require'lspconfig'.dockerls.setup{}
+require'lspconfig'.yamlls.setup{}
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
+}
+
+-- Load ColorScheme
+vim.cmd[[colorscheme tokyonight-night]]
 
 -- Show line numbers
 vim.wo.number = true
 
 -- Change indent to 2 spaces
-vim.o.shiftwidth = 2
+vim.bo.shiftwidth = 2
+
+-- OR set termguicolors to enable highlight groups
+vim.o.termguicolors = true
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- OR setup with some options
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- Use F2 to toggle NvimTree
+vim.api.nvim_set_keymap("n", "<F2>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- Change between day and night colorschemes
+vim.api.nvim_set_keymap('n', '<F8>', ":colorscheme tokyonight-day <CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F9>', ":colorscheme tokyonight-night <CR>", { noremap = true, silent = true })
+
+-- formatter.nvim setup
+require('formatter').setup({
+  logging = false, -- Set to true to enable debug logging
+  filetype = {
+    python = {
+      function()
+        return {
+          exe = 'black',
+          args = { '-' },
+          stdin = true,
+        }
+      end,
     },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
+    go = {
+      function()
+        return {
+          exe = 'gofmt',
+          args = {},
+          stdin = true,
+        }
+      end,
+    },
+    javascript = {
+      function()
+        return {
+          exe = 'prettier',
+          args = { '--stdin-filepath', vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote' },
+          stdin = true,
+        }
+      end,
+    },
+    typescript = {
+      function()
+        return {
+          exe = 'prettier',
+          args = { '--stdin-filepath', vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote' },
+          stdin = true,
+        }
+      end,
+    },
   },
 })
 
---Use F2 to toggle NerdTree
-vim.keymap.set("n","<F2>", ":NvimTreeToggle<CR>")
+-- Define an autocmd to run formatter.nvim on save
+vim.cmd([[augroup FormatAutogroup]])
+vim.cmd([[  autocmd!]])
+vim.cmd([[  autocmd BufWritePost * FormatWrite]])
+vim.cmd([[augroup END]])
+
+
 
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,38 +1,45 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
+return {
   -- nvim-lspconfig
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+  'neovim/nvim-lspconfig', -- Configurations for Nvim LSP
 
-  -- vim-surrounf
-  use 'tpope/vim-surround'
+  -- vim-surround
+  'tpope/vim-surround',
 
   -- nerdcommenter
-  use 'preservim/nerdcommenter'
+  'preservim/nerdcommenter',
 
   -- peekaboo
-  use 'junegunn/vim-peekaboo'
+  'junegunn/vim-peekaboo',
 
-  -- fugitive 
-  use 'tpope/vim-fugitive'
-  
-  --nvim-tree
-  use {
-    'nvim-tree/nvim-tree.lua',
+  -- fugitive
+  'tpope/vim-fugitive',
+
+  -- nvim-tree
+  {
+    'kyazdani42/nvim-tree.lua',
     requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
     },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
+    cmd = 'NvimTreeToggle',
+  },
 
+  -- TokyoNight Colorscheme
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup{}
+    end
+  },
 
-end)
+  -- fzf
+  {
+    'junegunn/fzf.vim',
+    requires = { 'junegunn/fzf', run = ':call fzf#install()' }
+  },
 
+  -- formatter
+  'mhartington/formatter.nvim'
 
-
+}
