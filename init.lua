a492753539e07e6f66fc6cda64d1fb53fbf51c83
Travis Kopp <travis.kopp@flypyka.com>
2026-01-19 20:31:37 -0800

    refresh to reflect changes to nvim-lsp

--- init.lua
+++ init.lua
@@ -1,115 +1,120 @@
 local plugins = require('plugins')
 
--- Bootstrap Lazy
+-- 1. BOOTSTRAP LAZY.NVIM
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
+    "git", "clone", "--filter=blob:none", "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git", lazypath,
   })
 end
 vim.opt.runtimepath:prepend(lazypath)
 
--- Load Lazy
+-- 2. LOAD PLUGINS
 require("lazy").setup(plugins, {
   termguicolors = true,
   disable_netrw = true,
 })
 
--- Mason setup
+-- 3. MASON & CAPABILITIES
 require("mason").setup()
-require("mason-lspconfig").setup ({
-  ensure_installed = { "lua_ls", "pyright", "gopls", "dockerls", "yamlls", "bashls", "eslint", "ts_ls", "solargraph", "rubocop", "clangd", "graphql", "terraformls" },
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls", "pyright", "gopls", "dockerls", "yamlls", "bashls",
+    "eslint", "ts_ls", "solargraph", "rubocop", "clangd", "graphql", "terraformls"
+  },
 })
 
--- Autocompletion capabilities
+-- Common capabilities for nvim-cmp
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- LSP setup
-local lspconfig = require('lspconfig')
-local servers = { "pyright", "gopls", "dockerls", "yamlls", "bashls", "ts_ls", "clangd", "graphql", "solargraph", "terraformls" }
+-- 4. NATIVE LSP CONFIGURATION (0.11 API)
+local servers = {
+  "pyright", "gopls", "dockerls", "yamlls", "bashls",
+  "ts_ls", "clangd", "graphql", "solargraph", "terraformls", "lua_ls"
+}
+
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup { capabilities = capabilities }
+  -- Fetch the default configuration from nvim-lspconfig data
+  local config = vim.lsp.config[lsp]
+
+  if config then
+    -- Merge your custom capabilities and flags into the default config
+    config = vim.tbl_deep_extend("force", config, {
+      capabilities = capabilities,
+      flags = { debounce_text_changes = 300 },
+    })
+
+    -- Server-Specific Logic (Clangd)
+    if lsp == "clangd" then
+      config.cmd = {
+        'clangd', '--background-index', '--clang-tidy', '--log=verbose',
+        '--header-insertion=iwyu', '--completion-style=detailed', '-j=4'
+      }
+      config.init_options = { fallbackFlags = { '-std=c++17' } }
+    end
+
+    -- Server-Specific Logic (Lua)
+    if lsp == "lua_ls" then
+      config.on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not (vim.uv or vim.loop).fs_stat(path..'/.luarc.json') and not (vim.uv or vim.loop).fs_stat(path..'/.luarc.jsonc') then
+          client.config.settings.Lua = {
+            runtime = { version = 'LuaJIT' },
+            workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } },
+          }
+        end
+        return true
+      end
+    end
+
+    -- Register the modified config
+    vim.lsp.config(lsp, config)
+  end
 end
 
--- ESLint setup
-lspconfig.eslint.setup {
-  capabilities = capabilities,
-  settings = {
-    format = { enable = true },
-    codeAction = { disableRuleComment = { enable = true } },
-  },
-  on_attach = function(client)
-    client.server_capabilities.documentFormattingProvider = true
+-- Enable all servers (This replaces lspconfig setup loops)
+vim.lsp.enable(servers)
+
+-- Diagnostics Setup
+vim.diagnostic.config({
+  virtual_text = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+-- 5. KEYBINDINGS (LspAttach Autocommand)
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf, silent = true }
+
+    -- Navigation
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K',  vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+
+    -- Actions
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
   end,
-  flags = {
-    debounce_text_changes = 150,
-  }
-}
+})
 
--- Enable LSP diagnostics to show in the buffer
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,  -- Show virtual text (error messages inline)
-    update_in_insert = false, -- Don't update diagnostics in insert mode
-  }
-)
-
-
--- Lua setup
-lspconfig.lua_ls.setup {
-  capabilities = capabilities,
-  on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-        Lua = {
-          runtime = { version = 'LuaJIT' },
-          workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } },
-        },
-      })
-      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-    end
-    return true
-  end,
-}
-
--- Clangd setup with extra features
-lspconfig.clangd.setup {
-  cmd = { 'clangd', '--background-index', '--clang-tidy', '--log=verbose' },
-  init_options = { fallbackFlags = { '-std=c++17' } },
-}
-
--- Rubocop setup
-vim.opt.signcolumn = "yes"
+-- 6. RUBOCOP (Manual Startup)
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "ruby",
   callback = function()
-    vim.lsp.start { name = "rubocop", cmd = { "bundle", "exec", "rubocop", "--lsp" } }
+    vim.lsp.start({ name = "rubocop", cmd = { "bundle", "exec", "rubocop", "--lsp" } })
   end,
 })
 
--- UI and extras
+-- 7. GENERAL EDITOR SETTINGS & UI
 vim.cmd[[colorscheme everforest]]
-
-vim.keymap.set("n", "s", require('substitute').operator, { noremap = true })
-vim.keymap.set("n", "ss", require('substitute').line, { noremap = true })
-vim.keymap.set("n", "S", require('substitute').eol, { noremap = true })
-vim.keymap.set("x", "s", require('substitute').visual, { noremap = true })
-
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-
--- Editor settings
+vim.o.background = "dark"
 vim.opt.hidden = true
 vim.wo.number = true
 vim.opt.shiftwidth = 2
@@ -120,54 +125,33 @@ vim.opt.autoindent = true
 vim.opt.termguicolors = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.opt.signcolumn = "yes"
+
+-- 8. KEYMAPS (General & Plugins)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {desc="Find References"})
+vim.keymap.set('n', '<leader>ft', function()
+    local function_name = vim.fn.expand("<cword>")
+    require('telescope.builtin').live_grep({
+        default_text = "def test_" .. function_name,
+    })
+end, { desc = 'Find tests for function under cursor' })
+
+-- Substitute Plugin
+vim.keymap.set("n", "s", require('substitute').operator, { noremap = true })
+vim.keymap.set("n", "ss", require('substitute').line, { noremap = true })
 
 -- Nvim-tree
-require("nvim-tree").setup {
-  sort = { sorter = "case_sensitive" },
-  view = { adaptive_size = true },
-  renderer = { group_empty = true },
-  filters = { dotfiles = true },
-}
+require("nvim-tree").setup { filters = { dotfiles = true } }
 vim.api.nvim_set_keymap("n", "<F2>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F8>', ":colorscheme tokyonight-day <CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F9>', ":colorscheme tokyonight-night <CR>", { noremap = true, silent = true })
 
--- Formatter
+-- Formatter Autocmd
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = { "*.ts", "*.js", "*.tsx", "*.jsx" },
   callback = function()
     vim.lsp.buf.format({ async = false })
   end,
 })
-
--- LSP mappings
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
-  end,
-})
-
-vim.cmd([[augroup FormatAutogroup]])
-vim.cmd([[  autocmd!]])
-vim.cmd([[  autocmd BufWritePost * FormatWrite]])
-vim.cmd([[augroup END]])
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -52,6 +52,45 @@ return {
   end,
 },
 
+-- Trouble.nvim
+{
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+},
+
 
   -- Everforest Colorscheme
   "sainnhe/everforest",
@@ -84,6 +123,11 @@ return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true
+  },
+
+  -- Whitespace
+  {
+    'ntpeters/vim-better-whitespace'
   },
 
   -- Completion engine
